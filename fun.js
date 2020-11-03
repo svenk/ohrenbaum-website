4d8fcaffc989766bd3715e4739016d082adee9d6
Sven Sveto <sven@sveto>
2020-11-03 18:22:46 +0100

    Ironie-Switcher weiter bearbeitet.

--- fun.js
+++ fun.js
@@ -13,12 +13,12 @@ var ironyLevel = defaultIronyLevel; /* current global level */
 
 var ironySwitcher = 
     ' <form>                              '+
-    ' <label for="irony">Content:</label> '+
+    ' <label for="irony">Inhalt:</label> '+
     ' <select name="irony" id="irony">    '+
-    '   <option value="0">Extremely serious</option>'+
-    '   <option value="1">Slightly winking</option> '+
-    '   <option value="2">Sense of humor</option>   '+
-    '   <option value="3">I forget myself</option>  '+
+    '   <option value="0">Extrem Ernst</option>'+
+    '   <option value="1">Ironisch</option> '+
+    '   <option value="2">Sarkastisch</option>   '+
+    '   <option value="3">Absurd</option>  '+
     ' </select>                           '+
     ' </form>';
     
@@ -32,6 +32,15 @@ function ready(fn) {
   }
 }
 
+// Vanilla JS function that changes a select element's option.
+function select(selectElement, optionValToSelect){
+    for(var opt, j = 0; opt = selectElement.options[j]; j++) {
+        if(opt.value == optionValToSelect) {
+            selectElement.selectedIndex = j; break;
+        }
+    }
+}
+
 function detectIrony() {
     // Will return the last finding of ironyLevel in the current URL.
     var m = window.location.href.match(ironyLevelRegexp);
@@ -39,20 +48,24 @@ function detectIrony() {
 }
 
 function scatterIrony(level) {
+    ironyLevel = level; // global storage
     console.log("Scattering level ", level);
     
     for(i=0; i<maxIronyLevel; i++) {
         document.querySelector("body").classList.remove("ironyLevel"+i);
-        if(i <= level)
-            document.querySelector("body").classList.add("ironyLevel"+i);
+        //if(i <= level)
+        //    document.querySelector("body").classList.add("ironyLevel"+i);
     }
+    document.querySelector("body").classList.add("ironyLevel"+level);
     
     document.querySelectorAll('a').forEach(function(e){
         if(e.href.match(ironyLevelRegexp))
-            e.href.replace(ironyLevelRegexp, "ironyLevel="+level.toString())
+            e.href = e.href.replace(ironyLevelRegexp, "ironyLevel="+level.toString())
         else
-            e.href += "#ironyLevel="+level.toString();
+            e.href += "#ironyLevel="+level.toString(); // this will probably destroy some deeplinks 
     });
+    
+    select(document.querySelector("nav select"), level);
 }
 
 
@@ -65,9 +78,13 @@ ready(function() {
     scatterIrony(ironyLevel);
     
     document.querySelector('nav select').addEventListener("input", function(e){
-        console.log(e.srcElement);
-        ironyLevel = e.srcElement.value; // populate global level
-        scatterIrony(ironyLevel);
+        scatterIrony(e.srcElement.value);
     });
     
+    document.querySelectorAll("a[data-switch-irony]").forEach(function(e) {
+        e.addEventListener("click", function() {
+            scatterIrony(e.getAttribute("data-switch-irony"));
+            return false;
+        });
+    });
 });
